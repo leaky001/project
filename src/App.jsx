4bc9e790f0b3aa45fb5bc4b2e73688ae13624e98
leaky001 <lakybass19@gmail.com>
2025-05-15 14:38:43 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import {AllRoutes} from './routes/AllRoutes'
-import {  Footer  } from './components'
-import {Header} from './components'
+import { AllRoutes } from './routes/AllRoutes'
+import { Footer } from './components/Layout/Footer'
+import { Header } from './components/Layout/Header'
 
 const App = () => {
   return (
     <div className='App dark:bg-dark'>
-      <Header/>
-      <AllRoutes/>
-      <Footer/>
+      <Header />
+      <AllRoutes />
+      <Footer />
     </div>
   )
 }
