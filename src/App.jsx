662b48205056f7f2c74ed056a4ffd9e9490fb3f7
leaky001 <lakybass19@gmail.com>
2025-05-15 14:03:01 +0100

    pro

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import AllRoutes from './routes/AllRoutes';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React from 'react'
+import {AllRoutes} from './routes/AllRoutes'
+import {  Footer  } from './components'
+import {Header} from './components'
 
 const App = () => {
   return (
     <div className='App dark:bg-dark'>
-      <Header />
-      <AllRoutes />
-      <Footer />
+      <Header/>
+      <AllRoutes/>
+      <Footer/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
